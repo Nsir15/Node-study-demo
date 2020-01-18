b58a8ce1a0de42502c091a1358013aec8ce6432b
南希 <447108518@qq.com>
2020-01-18 12:05:59 +0800

    修复 编码格式错误.添加json用于API请求练习

--- staticWeb.js
+++ staticWeb.js
@@ -9,14 +9,15 @@ let handle = function(req,res){
         pathReg = /\.([a-z0-9]+)$/i,
         suffix = pathReg.test(pathname) ? pathReg.exec(pathname)[1] : null,
         encodeReg = /(JPG|JPEG|PNG|GIF|SVG|ICO|BMP|EOT|TTF|WOFF|MP3|MP4|OGG|WAV|M4A|WMV|AVI)$/i,
-        encoding = encodeReg.test(suffix) ? ':charset=utf-8':'';
+        // 富媒体资源就不加编码格式了
+        encoding = !encodeReg.test(suffix) ? 'charset=utf-8':null;
     
     if(suffix){ // 请求静态资源
         pathname = path.resolve('./static') + pathname;
        _readFile(pathname).then(result => {
 
          res.writeHead(200, {
-           'content-type': `${mime.getType(suffix)}${encoding}`
+           'content-type': `${mime.getType(suffix)};${encoding}`
          })
          res.end(result)
        }).catch(error => {
